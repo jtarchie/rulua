a4d38510af97db8791f1e174177ac1f00472d6d9
JT Archie <jtarchie@gmail.com>
2018-12-25 11:05:43 -0500

    support print

--- spec/integration/lua_spec.rb
+++ spec/integration/lua_spec.rb
@@ -1,20 +1,29 @@
 require 'rufus-lua'
 require 'spec_helper'
+require 'tempfile'
 require_relative '../../lib/rulua'
 
 RSpec.describe 'When specifying a lua function' do
-  def compile(&block)
+  def compile(process: false, &block)
     expect do
       node = RubyVM::AbstractSyntaxTree.of(block)
       expect(node).to_not be_nil
 
       lua = Rulua.tranverse(node)
-      state = Rufus::Lua::State.new
-      state.eval(lua)
+
+      if process
+        file = Tempfile.new
+        file.write lua
+        file.close
+        system("lua #{file.path}")
+      else
+        state = Rufus::Lua::State.new
+        state.eval(lua)
+      end
     end
   end
 
-  context ':lua.assert' do
+  context '#assert' do
     it 'succeeds on a true assertion' do
       compile do
         :lua.assert(true, 'message')
@@ -25,6 +34,14 @@ RSpec.describe 'When specifying a lua function' do
       compile do
         :lua.assert(false, 'message')
       end.to raise_error(/message/)
+    end
+  end
+
+  context '#print' do
+    it 'outputs to stdout' do
+      compile(process: true) do
+        :lua.print('hello, world')
+      end.to output("hello, world\n").to_stdout_from_any_process
     end
   end
 end
